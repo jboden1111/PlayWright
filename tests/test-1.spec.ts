5fef67da89468f4a78790f3ed165065d53955016
jboden1111 <jboden@bodenautomation.com>
2025-09-21 13:42:01 -0500

    test of ignore

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,6 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
     await page.goto('https://www.twinspires.com/bet/program/classic/horseshoe-indianapolis/ind/Thoroughbred/1/basic/');
-
-    
+  
 });
