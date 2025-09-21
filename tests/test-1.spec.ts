730ff5f681c888d5513b436e812842abcb2529c9
jboden1111 <jboden@bodenautomation.com>
2025-09-21 13:32:26 -0500

    Just A Test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-await page.goto('https://www.twinspires.com/bet/program/classic/horseshoe-indianapolis/ind/Thoroughbred/1/basic/');
+    await page.goto('https://www.twinspires.com/bet/program/classic/horseshoe-indianapolis/ind/Thoroughbred/1/basic/');
 
+    
 });
