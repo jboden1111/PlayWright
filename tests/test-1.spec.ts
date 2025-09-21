49b15f05580dee0185ca67572540a550bb137e09
jboden1111 <154277110+jboden1111@users.noreply.github.com>
2025-09-21 14:07:13 -0500

    Update test-1.spec.ts

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,12 +2,4 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
     await page.goto('https://www.twinspires.com/bet/program/classic/horseshoe-indianapolis/ind/Thoroughbred/1/basic/');
-
-<<<<<<< HEAD
-
-
-
-=======
-    
->>>>>>> 730ff5f681c888d5513b436e812842abcb2529c9
 });
